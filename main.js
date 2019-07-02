978466c04d4c4a709b0caa0e5c309a57928f9e85
Darrell Scrogham <dscrogham@gmail.com>
2019-07-02 14:53:57 -0400

    completed lesson

--- main.js
+++ main.js
@@ -143,3 +143,79 @@ In .addRating(), make sure input is between 1 and 5.
 Create a method called shuffle for the CD class. The method returns a randomly sorted array of all the songs in the songs property.
 Create class called Catalog that holds all of the Media items in our library.
 */
+
+class Media {
+    constructor(title){
+       this._title = title;
+      this._isCheckedOut = false;
+      this._ratings = [];
+    }
+     get title() {
+      return this._title;
+    }
+    get isCheckedOut() {
+      return this._isCheckedOut;
+    }
+    get ratings() {
+      return this._ratings;
+    }
+   set isCheckedOut(checkedOut) {
+      this._isCheckedOut = checkedOut; 
+   }
+     toggleCheckOutStatus() {
+      this.isCheckedOut = !this.isCheckedOut;
+    }
+     getAverageRating() {
+       let ratingsSum =  this.ratings.reduce((accumulator, rating) => accumulator + rating); 
+       return ratingsSum / this.ratings.length;
+     }
+    addRating(value) {
+      this.ratings.push(value);
+  } 
+  }
+  
+   class Book extends Media {
+       constructor(author, title, pages) {
+        super(title);
+  this._author = author;
+  this._pages = pages;  
+     }
+     get author() {
+    return this._author;
+  }
+  get pages() {
+    return this._pages;
+  }
+   }
+  
+  class Movie extends Media  {
+     constructor( director, title, runTime) {
+       super(title);
+       this._director = director;
+       this._runTime = runTime;
+     } 
+     get director() {
+       return this._director;
+     }
+     get runTime() {
+       return this._runTime;
+     }
+  }
+  
+  const historyOfEverything = new Book( 'Bill Bryson', 'A Short History of Nearly Everything', 544);
+  historyOfEverything.toggleCheckOutStatus();
+  console.log(historyOfEverything.isCheckedOut); // true
+  historyOfEverything.addRating(4);
+  historyOfEverything.addRating(5);
+  historyOfEverything.addRating(5);
+  console.log(historyOfEverything.getAverageRating()); // 4.6666666666666
+  
+  const speed = new Movie('Jan de Bont', 'Speed', 116);
+  speed.toggleCheckOutStatus();
+  console.log(speed.isCheckedOut); // true
+  speed.addRating(1);
+  speed.addRating(1);
+  speed.addRating(5);
+  speed.addRating(3);
+  console.log(speed.getAverageRating()); // 2.5
+  
